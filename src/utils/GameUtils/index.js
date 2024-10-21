644d4b3e869bc72c311344f8f163e703f0239a74
Mohau Mphanya <mphanyamohau28@gmail.com>
2024-10-21 21:08:24 +0200

    fixed checkForWinner function

--- src/utils/GameUtils/index.js
+++ src/utils/GameUtils/index.js
@@ -1,39 +1,38 @@
 const checkForSequence = (option1, option2, option3) => {
-    return option1 === option2 && option2 === option3
-}
+    return option1 !== null && option1 === option2 && option2 === option3;
+};
 
 export const checkForWinner = (board) => {
-    console.log("in here")
-    // 0 1 2
-    // 3 4 5
-    // 6 7 8
-
-    //Rows
-    for(let i; i<9; i+=3) {
-        if(checkForSequence(board[i], board[i+1], board[i+2])) {
-            console.log("row winner")
-            return true
+    console.log("in here");
+    // Rows
+    for (let i = 0; i < 9; i += 3) {
+        if (checkForSequence(board[i], board[i+1], board[i+2])) {
+            console.log("row winner");
+            return true;
         }
     }
 
-    //Columns
-    for(let i=0; i<3; i+=1) {
-        if(checkForSequence(board[i], board[i+3], board[i+6])) {
-            console.log("column winner")
-            return true
+    // Columns
+    for (let i = 0; i < 3; i++) {
+        if (checkForSequence(board[i], board[i+3], board[i+6])) {
+            console.log("column winner");
+            return true;
         }
     }
 
     // Diagonals
     // Diagonal 1
-    if (board[0] === board[4] && board[4] === board[8]) {
-        return true
-        console.log("winner")
+    if (checkForSequence(board[0], board[4], board[8])) {
+        console.log("diagonal winner");
+        return true;
     }
 
     // Diagonal 2
-    if (board[2] === board[4] && board[4] === board[6]) {
-        return true
-        console.log("winner")
+    if (checkForSequence(board[2], board[4], board[6])) {
+        console.log("diagonal winner");
+        return true;
     }
-}
+
+    // No winner found
+    return false;
+};
